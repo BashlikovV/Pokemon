2c10be8a885eb91c3eab3a413caf0b276337f7ad
bashlikovvv <volodia413314@gmail.com>
2023-07-21 11:00:36 +0300

    removed.

--- app/src/main/java/by/bashlikovvv/pokemon/domain/model/ability/Ability.kt
+++
@@ -1,8 +0,0 @@
-package by.bashlikovvv.pokemon.domain.model.ability
-
-import com.google.gson.annotations.SerializedName
-
-data class Ability(
-    @SerializedName("name") private val abilityName: String,
-    @SerializedName("url") private val abilityUrl: String
-)
--- app/src/main/java/by/bashlikovvv/pokemon/domain/model/type/PokemonType.kt
+++
@@ -1,9 +0,0 @@
-package by.bashlikovvv.pokemon.domain.model.type
-
-import com.google.gson.annotations.SerializedName
-
-data class PokemonType constructor(
-    @SerializedName("slot") private val typeSlot: Int,
-    @SerializedName("name") private val typeName: String,
-    @SerializedName("url") private val typeUrl: String
-)
