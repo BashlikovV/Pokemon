5eff857a860777537dfc8b9fb1691e2a1a82e1a9
bashlikovvv <volodia413314@gmail.com>
2023-07-26 23:15:30 +0300

    renamed

--- app/src/main/java/by/bashlikovvv/pokemon/domain/model/PokemonItem.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/domain/model/PokemonItem.kt
@@ -2,9 +2,8 @@ package by.bashlikovvv.pokemon.domain.model
 
 import android.graphics.Bitmap
 
-data class Pokemon(
+data class PokemonItem(
     val name: String,
     val id: Long,
-    val image: Bitmap,
-    val sprite: Bitmap
+    val sprites: Map<String, Bitmap>
 )
