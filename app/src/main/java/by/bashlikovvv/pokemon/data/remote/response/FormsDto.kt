d4022f84390b8c1c2e48d366229ac12f52cf972e
bashlikovvv <volodia413314@gmail.com>
2023-07-26 23:09:19 +0300

    Responses added.

---
+++ app/src/main/java/by/bashlikovvv/pokemon/data/remote/response/FormsDto.kt
@@ -0,0 +1,8 @@
+package by.bashlikovvv.pokemon.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class FormsDto(
+    @SerializedName("name") val name: String?,
+    @SerializedName("url") val url: String?
+)
