eebf2886c37c05e7780bcc18a66411954d35dd90
bashlikovvv <volodia413314@gmail.com>
2023-07-26 23:12:22 +0300

    Response added.

--- app/src/main/java/by/bashlikovvv/pokemon/data/remote/response/OtherDto.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/data/remote/response/OtherDto.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class OtherDto(
     @SerializedName("dream_world") val dreamWorld: DreamWorldDto,
     @SerializedName("home") val home: HomeDto,
-    @SerializedName("official-artwork") val officialArtwork: OfficialArtwork
+    @SerializedName("official-artwork") val officialArtworkDto: OfficialArtworkDto
 )
