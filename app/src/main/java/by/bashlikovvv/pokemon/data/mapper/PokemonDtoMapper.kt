fd16561ed5c6179c159114f1c9a874cefb26890b
bashlikovvv <volodia413314@gmail.com>
2023-07-28 14:49:26 +0300

    PokemonDtoMapper updated.

--- app/src/main/java/by/bashlikovvv/pokemon/data/mapper/PokemonDtoMapper.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/data/mapper/PokemonDtoMapper.kt
@@ -2,23 +2,24 @@ package by.bashlikovvv.pokemon.data.mapper
 
 import by.bashlikovvv.pokemon.data.remote.response.PokemonDto
 import by.bashlikovvv.pokemon.domain.model.PokemonItem
-import by.bashlikovvv.pokemon.domain.model.Sprites
 import by.bashlikovvv.pokemon.utils.Constants.Companion.BASE_URL
 
-class PokemonDtoMapper(private val sprites: Sprites): Mapper<PokemonDto, PokemonItem> {
+class PokemonDtoMapper : Mapper<PokemonDto, PokemonItem> {
+
     override fun mapFromEntity(entity: PokemonDto): PokemonItem {
-        val id = entity.url
-            .replace("${BASE_URL}pokemon/", "")
-            .replace("/", "")
-            .toInt()
         return PokemonItem(
             name = entity.name,
-            id = id.toLong(),
-            sprites = sprites.sprites
+            id = entity.getId(),
+            sprites = mutableMapOf()
         )
     }
 
     override fun mapToEntity(domain: PokemonItem): PokemonDto {
         throw UnsupportedOperationException("Not implemented")
     }
+
+    private fun PokemonDto.getId(): Int = this.url
+        .replace("${BASE_URL}pokemon/", "")
+        .replace("/", "")
+        .toInt()
 }
