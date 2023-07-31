e1bff3b839ac237fce80abf7786896c6a22e0e00
bashlikovvv <volodia413314@gmail.com>
2023-07-31 12:34:37 +0300

    OfficialArtworkDto updated.

--- app/src/main/java/by/bashlikovvv/pokemon/data/remote/response/OfficialArtworkDto.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/data/remote/response/OfficialArtworkDto.kt
@@ -3,6 +3,6 @@ package by.bashlikovvv.pokemon.data.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class OfficialArtworkDto(
-    @SerializedName("front_default") val frontDefault: String?,
-    @SerializedName("front_shiny") val frontShiny: String?
+    @SerializedName("front_default") val frontDefault: String,
+    @SerializedName("front_shiny") val frontShiny: String
 )
