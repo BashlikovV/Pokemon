4c9bbb34f4a375bcd22601f8e0eb42ac3639ec33
bashlikovvv <volodia413314@gmail.com>
2023-07-31 12:35:01 +0300

    PokemonDetails updated.

--- app/src/main/java/by/bashlikovvv/pokemon/domain/model/PokemonDetails.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/domain/model/PokemonDetails.kt
@@ -31,5 +31,32 @@ sealed class SpriteNames(val name: String) {
 
     class FrontShiny : SpriteNames("frontShiny")
 
+    sealed class Other(val component0: String) : SpriteNames("other") {
 
+        sealed class DreamWorld(val component1: String) : Other("dreamWorld") {
+
+            class FrontDefault : DreamWorld("frontDefault")
+
+            class FrontFemale : DreamWorld("frontFemale")
+        }
+
+        sealed class Home(val component1: String) : Other("home") {
+
+            class FrontDefault : Home("frontDefault")
+
+            class FrontFemale : Home("frontFemale")
+
+            class FrontShiny : DreamWorld("frontShiny")
+
+            class FrontShinyFemale : DreamWorld("frontShinyFemale")
+        }
+
+        sealed class OfficialArtwork(val component1: String) : Other("officialArtwork") {
+
+            class FrontDefault : OfficialArtwork("frontDefault")
+
+            class FrontShiny : OfficialArtwork("frontShiny")
+
+        }
+    }
 }
