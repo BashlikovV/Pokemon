84f32372409805b733b441e7299fa4dfc7c715f8
bashlikovvv <volodia413314@gmail.com>
2023-07-31 12:33:11 +0300

    HomeDto updated.

--- app/src/main/java/by/bashlikovvv/pokemon/data/remote/response/HomeDto.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/data/remote/response/HomeDto.kt
@@ -3,8 +3,8 @@ package by.bashlikovvv.pokemon.data.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class HomeDto(
-    @SerializedName("front_default") val frontDefault: String?,
-    @SerializedName("front_female") val frontFemale: Any?,
-    @SerializedName("front_shiny") val frontShiny: String?,
-    @SerializedName("front_shiny_female") val frontShinyFemale: String?
+    @SerializedName("front_default") val frontDefault: String,
+    @SerializedName("front_female") val frontFemale: String,
+    @SerializedName("front_shiny") val frontShiny: String,
+    @SerializedName("front_shiny_female") val frontShinyFemale: String
 )
