159472090e0d2dd1d59fdc404d98f8526c61f044
bashlikovvv <volodia413314@gmail.com>
2023-08-03 17:40:34 +0300

    Code cleaned,

--- app/src/main/java/by/bashlikovvv/pokemon/utils/BitmapUtils.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/utils/BitmapUtils.kt
@@ -4,13 +4,6 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import androidx.core.content.ContextCompat
-import by.bashlikovvv.pokemon.R
-import by.bashlikovvv.pokemon.data.di.DataModule
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.util.concurrent.ExecutionException
 
 fun Int.getBitmapFromImage(context: Context): Bitmap {
     val db = ContextCompat.getDrawable(context, this)
@@ -22,21 +15,4 @@ fun Int.getBitmapFromImage(context: Context): Bitmap {
     db.draw(canvas)
 
     return bit
-}
-
-suspend fun getBitmapWithGlide(url: String) = withContext(Dispatchers.IO) {
-    return@withContext try {
-        val result = Glide.with(DataModule.applicationContext!!)
-            .asBitmap()
-            .load(url)
-            .transform(CenterCrop())
-            .submit()
-            .get()
-
-        result ?: R.drawable.baseline_error_24.getBitmapFromImage(DataModule.applicationContext!!)
-    } catch (e: ExecutionException) {
-        R.drawable.baseline_error_24.getBitmapFromImage(DataModule.applicationContext!!)
-    } catch (e: InterruptedException) {
-        R.drawable.baseline_error_24.getBitmapFromImage(DataModule.applicationContext!!)
-    }
 }
