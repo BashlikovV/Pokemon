3bb8a6750ca0726ef478173016873fd411baf23c
bashlikovvv <volodia413314@gmail.com>
2023-08-03 17:42:33 +0300

    PokemonDetailsEntity added.

---
+++ app/src/main/java/by/bashlikovvv/pokemon/data/local/model/PokemonDetailsEntity.kt
@@ -0,0 +1,19 @@
+package by.bashlikovvv.pokemon.data.local.model
+
+import android.graphics.Bitmap
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import by.bashlikovvv.pokemon.data.local.contract.RoomContract.DetailsTable
+
+@Entity(tableName = DetailsTable.TABLE_NAME)
+data class PokemonDetailsEntity(
+    @ColumnInfo(name = DetailsTable.COLUMN_NAME) val name: String,
+    @ColumnInfo(name = DetailsTable.COLUMN_SPRITE) val sprite: Bitmap,
+    @ColumnInfo(name = DetailsTable.COLUMN_TYPES) val types: List<String>,
+    @ColumnInfo(name = DetailsTable.COLUMN_WEIGHT_IN_HG) val weightInHg: Int,
+    @ColumnInfo(name = DetailsTable.COLUMN_HEIGHT_IN_DM) val heightInDm: Int,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = DetailsTable.COLUMN_ID)
+    val id: Int
+)
