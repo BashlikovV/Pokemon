807e818717690af350d3fe7da7a989bca43fb27f
bashlikovvv <volodia413314@gmail.com>
2023-08-07 20:25:11 +0300

    PokemonItemEntityMapper added.

---
+++ app/src/main/java/by/bashlikovvv/pokemon/data/mapper/PokemonItemEntityMapper.kt
@@ -0,0 +1,26 @@
+package by.bashlikovvv.pokemon.data.mapper
+
+import by.bashlikovvv.pokemon.R
+import by.bashlikovvv.pokemon.data.di.DataModule
+import by.bashlikovvv.pokemon.data.local.model.PokemonItemEntity
+import by.bashlikovvv.pokemon.domain.model.PokemonItem
+import by.bashlikovvv.pokemon.domain.model.SpriteNames
+import by.bashlikovvv.pokemon.utils.getBitmapFromImage
+
+class PokemonItemEntityMapper : Mapper<PokemonItemEntity, PokemonItem> {
+    override fun mapFromEntity(entity: PokemonItemEntity): PokemonItem {
+        return PokemonItem(
+            name = entity.name,
+            id = entity.id,
+            sprites = mutableMapOf(SpriteNames.FrontShiny().name to entity.sprite)
+        )
+    }
+
+    override fun mapToEntity(domain: PokemonItem): PokemonItemEntity {
+        return PokemonItemEntity(
+            name = domain.name,
+            sprite = domain.sprites[SpriteNames.FrontShiny().name] ?: R.drawable.baseline_error_24.getBitmapFromImage(DataModule.applicationContext!!),
+            id = domain.id
+        )
+    }
+}
