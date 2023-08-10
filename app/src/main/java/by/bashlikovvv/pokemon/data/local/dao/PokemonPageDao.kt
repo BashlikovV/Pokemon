6e30f02ec67bcb751cc1793a84a54b97d7c9e8e8
bashlikovvv <volodia413314@gmail.com>
2023-08-10 21:55:17 +0300

    PokemonPageDao updated for using paging, selectItemsOffline method query updated.

--- app/src/main/java/by/bashlikovvv/pokemon/data/local/dao/PokemonPageDao.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/data/local/dao/PokemonPageDao.kt
@@ -1,9 +1,12 @@
 package by.bashlikovvv.pokemon.data.local.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
+import by.bashlikovvv.pokemon.data.local.contract.RoomContract.DetailsTable
 import by.bashlikovvv.pokemon.data.local.contract.RoomContract.PageTable
 import by.bashlikovvv.pokemon.data.local.model.PokemonItemEntity
 
@@ -11,11 +14,14 @@ import by.bashlikovvv.pokemon.data.local.model.PokemonItemEntity
 interface PokemonPageDao {
 
     @Query("SELECT * FROM ${PageTable.TABLE_NAME}")
-    suspend fun selectItemsOnline(): List<PokemonItemEntity>
+    fun selectItemsOnline(): PagingSource<Int, PokemonItemEntity>
 
-    /*@Transaction
-    @Query("SELECT * FROM ${PageTable.TABLE_NAME} WHERE ${PageTable.COLUMN_ID} IN (SELECT details_id FROM details)")
-    suspend fun selectItemsOffline(): List<PokemonItemEntity>*/
+    @Transaction
+    @Query("SELECT * " +
+           "FROM ${PageTable.TABLE_NAME} " +
+           "WHERE ${PageTable.COLUMN_ID} " +
+           "IN (SELECT ${DetailsTable.COLUMN_ID} FROM ${DetailsTable.TABLE_NAME})")
+    fun selectItemsOffline(): PagingSource<Int, PokemonItemEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertItem(itemEntity: PokemonItemEntity)
