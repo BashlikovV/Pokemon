dc28fcfed35be0f25eb7a115462fca06c0e282f4
bashlikovvv <volodia413314@gmail.com>
2023-08-15 17:23:41 +0300

    hilt and ksp support added, kapt replaced by ksp.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,26 +2,25 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
-    id("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "by.bashlikovvv.pokemon"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "by.bashlikovvv.pokemon"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        kapt {
-            arguments {
-                arg("room.schemaLocation", "$projectDir/schemas")
-            }
+        ksp {
+            arg("room.schemaLocation", "$projectDir/schemas")
         }
     }
 
@@ -72,16 +71,14 @@ dependencies {
     //Paging
     implementation ("androidx.paging:paging-runtime-ktx:3.2.0")
     //Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
     //Room
     implementation("androidx.room:room-runtime:2.5.2")
     implementation("androidx.room:room-ktx:2.5.2")
     implementation("androidx.room:room-paging:2.5.2")
-    kapt("androidx.room:room-compiler:2.5.2")
-
-}
-
-kapt {
-    correctErrorTypes = true
+    ksp("androidx.room:room-compiler:2.5.2")
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.45")
+    ksp("com.google.dagger:hilt-compiler:2.45")
 }
