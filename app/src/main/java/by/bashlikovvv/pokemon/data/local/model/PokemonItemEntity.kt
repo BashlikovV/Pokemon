a448a934bf9fded53f5ee671141ac5f27ec379a6
bashlikovvv <volodia413314@gmail.com>
2023-08-20 15:23:37 +0300

    image type changed.

--- app/src/main/java/by/bashlikovvv/pokemon/data/local/model/PokemonItemEntity.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/data/local/model/PokemonItemEntity.kt
@@ -1,6 +1,5 @@
 package by.bashlikovvv.pokemon.data.local.model
 
-import android.graphics.Bitmap
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -9,7 +8,7 @@ import by.bashlikovvv.pokemon.data.local.contract.RoomContract.PageTable
 @Entity(tableName = PageTable.TABLE_NAME)
 data class PokemonItemEntity(
     @ColumnInfo(name = PageTable.COLUMN_NAME) val name: String,
-    @ColumnInfo(name = PageTable.COLUMN_SPRITE) val sprite: Bitmap,
+    @ColumnInfo(name = PageTable.COLUMN_SPRITE) val sprite: String,
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = PageTable.COLUMN_ID) val id: Int
 )
