bea205c80634a9692aca222387dee6b21a1f8e4f
bashlikovvv <volodia413314@gmail.com>
2023-08-20 15:21:49 +0300

    image type changed;

--- app/src/main/java/by/bashlikovvv/pokemon/data/local/model/PokemonDetailsEntity.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/data/local/model/PokemonDetailsEntity.kt
@@ -1,6 +1,5 @@
 package by.bashlikovvv.pokemon.data.local.model
 
-import android.graphics.Bitmap
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -9,7 +8,7 @@ import by.bashlikovvv.pokemon.data.local.contract.RoomContract.DetailsTable
 @Entity(tableName = DetailsTable.TABLE_NAME)
 data class PokemonDetailsEntity(
     @ColumnInfo(name = DetailsTable.COLUMN_NAME) val name: String,
-    @ColumnInfo(name = DetailsTable.COLUMN_SPRITE) val sprite: Bitmap,
+    @ColumnInfo(name = DetailsTable.COLUMN_SPRITE) val sprites: List<String>,
     @ColumnInfo(name = DetailsTable.COLUMN_TYPES) val types: List<String>,
     @ColumnInfo(name = DetailsTable.COLUMN_WEIGHT_IN_HG) val weightInHg: Int,
     @ColumnInfo(name = DetailsTable.COLUMN_HEIGHT_IN_DM) val heightInDm: Int,
