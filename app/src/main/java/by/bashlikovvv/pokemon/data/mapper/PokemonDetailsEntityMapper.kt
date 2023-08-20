2c519b7861c65eeb6b42244127574fab68481e9a
bashlikovvv <volodia413314@gmail.com>
2023-08-20 15:22:30 +0300

    image type changed;

--- app/src/main/java/by/bashlikovvv/pokemon/data/mapper/PokemonDetailsEntityMapper.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/data/mapper/PokemonDetailsEntityMapper.kt
@@ -1,21 +1,15 @@
 package by.bashlikovvv.pokemon.data.mapper
 
-import android.content.Context
-import by.bashlikovvv.pokemon.R
 import by.bashlikovvv.pokemon.data.local.model.PokemonDetailsEntity
 import by.bashlikovvv.pokemon.domain.model.PokemonDetails
-import by.bashlikovvv.pokemon.domain.model.SpriteNames
 import by.bashlikovvv.pokemon.domain.model.Sprites
-import by.bashlikovvv.pokemon.utils.getBitmapFromImage
 
-class PokemonDetailsEntityMapper(
-    private val context: Context
-) : Mapper<PokemonDetailsEntity, PokemonDetails> {
+class PokemonDetailsEntityMapper : Mapper<PokemonDetailsEntity, PokemonDetails> {
     override fun mapFromEntity(entity: PokemonDetailsEntity): PokemonDetails {
         return PokemonDetails(
             id = entity.id,
             name = entity.name,
-            sprites = Sprites(mapOf(SpriteNames.FrontShiny().name to entity.sprite)),
+            sprites = Sprites(entity.sprites.associateBy { "" }),
             types = entity.types,
             weightInHg = entity.weightInHg,
             heightInDm = entity.heightInDm
@@ -23,13 +17,17 @@ class PokemonDetailsEntityMapper(
     }
 
     override fun mapToEntity(domain: PokemonDetails): PokemonDetailsEntity {
-        val sprite = domain.sprites.sprites[SpriteNames.FrontShiny().name] ?:
-            R.drawable.baseline_error_24.getBitmapFromImage(context)
+        val sprites = mutableListOf<String>()
+        domain.sprites.sprites.forEach {
+            if (it.value != null) {
+                sprites.add(it.value!!)
+            }
+        }
 
         return PokemonDetailsEntity(
             id = domain.id,
             name = domain.name,
-            sprite = sprite,
+            sprites = sprites,
             types = domain.types,
             weightInHg = domain.weightInHg,
             heightInDm = domain.heightInDm
