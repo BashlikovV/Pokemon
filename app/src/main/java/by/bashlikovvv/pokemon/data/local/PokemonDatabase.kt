0e2ab64dd46f9e66c091ff5498460b07fc5f5135
bashlikovvv <volodia413314@gmail.com>
2023-08-20 15:21:31 +0300

    Unused converters removed.

--- app/src/main/java/by/bashlikovvv/pokemon/data/local/PokemonDatabase.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/data/local/PokemonDatabase.kt
@@ -3,9 +3,7 @@ package by.bashlikovvv.pokemon.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import by.bashlikovvv.pokemon.data.local.converters.BitmapTypeConverter
 import by.bashlikovvv.pokemon.data.local.converters.ListTypeConverter
-import by.bashlikovvv.pokemon.data.local.converters.StringTypeConverter
 import by.bashlikovvv.pokemon.data.local.dao.PokemonDetailsDao
 import by.bashlikovvv.pokemon.data.local.dao.PokemonPageDao
 import by.bashlikovvv.pokemon.data.local.model.PokemonDetailsEntity
@@ -15,7 +13,7 @@ import by.bashlikovvv.pokemon.data.local.model.PokemonItemEntity
     entities = [PokemonItemEntity::class, PokemonDetailsEntity::class],
     version = 1
 )
-@TypeConverters(BitmapTypeConverter::class, StringTypeConverter::class, ListTypeConverter::class)
+@TypeConverters(ListTypeConverter::class)
 abstract class PokemonDatabase : RoomDatabase() {
     abstract val pokemonDetailsDao: PokemonDetailsDao
     abstract val pokemonPageDao: PokemonPageDao
