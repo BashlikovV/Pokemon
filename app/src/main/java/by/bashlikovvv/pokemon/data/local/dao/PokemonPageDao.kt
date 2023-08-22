7679e6c9c1201285e16eddd24560a2f9fc6e7e8b
bashlikovvv <volodia413314@gmail.com>
2023-08-22 15:20:18 +0300

    Code cleaned, updateIsSelectedOnline, updateIsSelectedOffline and unselectPokemon methods added.

--- app/src/main/java/by/bashlikovvv/pokemon/data/local/dao/PokemonPageDao.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/data/local/dao/PokemonPageDao.kt
@@ -6,9 +6,11 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Update
 import by.bashlikovvv.pokemon.data.local.contract.RoomContract.DetailsTable
 import by.bashlikovvv.pokemon.data.local.contract.RoomContract.PageTable
 import by.bashlikovvv.pokemon.data.local.model.PokemonItemEntity
+import by.bashlikovvv.pokemon.data.local.tuple.UpdatePokemonSelectedTuple
 
 @Dao
 interface PokemonPageDao {
@@ -17,10 +19,15 @@ interface PokemonPageDao {
     fun selectItemsOnline(): PagingSource<Int, PokemonItemEntity>
 
     @Transaction
-    @Query("SELECT * " +
-           "FROM ${PageTable.TABLE_NAME} " +
-           "WHERE ${PageTable.COLUMN_ID} " +
-           "IN (SELECT ${DetailsTable.COLUMN_ID} FROM ${DetailsTable.TABLE_NAME})")
+    @Query(
+        "SELECT * " +
+        "FROM ${PageTable.TABLE_NAME} " +
+        "WHERE ${PageTable.COLUMN_ID} " +
+        "IN (" +
+            "SELECT ${DetailsTable.COLUMN_ID} " +
+            "FROM ${DetailsTable.TABLE_NAME}" +
+        ")"
+    )
     fun selectItemsOffline(): PagingSource<Int, PokemonItemEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -28,4 +35,31 @@ interface PokemonPageDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertItems(itemEntities: List<PokemonItemEntity>)
+
+    @Update(entity = PokemonItemEntity::class, onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateIsSelectedOnline(tuple: UpdatePokemonSelectedTuple)
+
+    @Query(
+        "UPDATE ${PageTable.TABLE_NAME} " +
+        "SET ${PageTable.COLUMN_SELECTED} = :selected " +
+        "WHERE ${PageTable.COLUMN_ID} " +
+        "IN (" +
+            "SELECT ${DetailsTable.COLUMN_ID} " +
+            "FROM ${DetailsTable.TABLE_NAME} " +
+            "WHERE ${DetailsTable.COLUMN_ID} = :id" +
+        ")"
+    )
+    suspend fun updateIsSelectedOffline(selected: Boolean, id: Int)
+
+    @Query(
+        "UPDATE ${PageTable.TABLE_NAME} " +
+        "SET ${PageTable.COLUMN_SELECTED} = ${false} " +
+        "WHERE ${PageTable.COLUMN_ID} " +
+        "IN (" +
+            "SELECT ${PageTable.COLUMN_ID} " +
+            "FROM ${PageTable.TABLE_NAME} " +
+            "WHERE ${PageTable.COLUMN_SELECTED} = ${true}" +
+        ")"
+    )
+    suspend fun unselectPokemon()
 }
