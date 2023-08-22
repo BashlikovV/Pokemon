1f18a883d9e2f39f034b7987a1a181a416fe9d8a
bashlikovvv <volodia413314@gmail.com>
2023-08-22 15:18:34 +0300

    SearchView in application bar added, back press processing added.

--- app/src/main/java/by/bashlikovvv/pokemon/presentation/view/PokemonListFragment.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/presentation/view/PokemonListFragment.kt
@@ -2,12 +2,20 @@ package by.bashlikovvv.pokemon.presentation.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.widget.SearchView
 import androidx.core.os.bundleOf
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
@@ -16,14 +24,14 @@ import by.bashlikovvv.pokemon.databinding.FragmentPokemonListBinding
 import by.bashlikovvv.pokemon.domain.model.PokemonItem
 import by.bashlikovvv.pokemon.presentation.adapters.PokemonListAdapter
 import by.bashlikovvv.pokemon.presentation.adapters.PokemonLoaderAdapter
-import by.bashlikovvv.pokemon.presentation.adapters.UserActionListener
+import by.bashlikovvv.pokemon.presentation.contract.IUserActionListener
 import by.bashlikovvv.pokemon.presentation.viewmodel.PokemonListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class PokemonListFragment : Fragment(), UserActionListener  {
+class PokemonListFragment : Fragment(), IUserActionListener {
 
     private var _binding: FragmentPokemonListBinding? = null
     private val binding: FragmentPokemonListBinding get() = _binding!!
@@ -33,6 +41,9 @@ class PokemonListFragment : Fragment(), UserActionListener  {
     private val adapter: PokemonListAdapter by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
         PokemonListAdapter(requireContext())
     }
+
+    private var _searchView: SearchView? = null
+    private val searchView get() = _searchView!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -64,6 +75,64 @@ class PokemonListFragment : Fragment(), UserActionListener  {
     private fun setUpFragment() {
         (requireActivity() as PokemonActivity).apply {
             supportActionBar?.title = getText(R.string.app_name)
+            onBackPressedDispatcher.addCallback(
+                owner = viewLifecycleOwner,
+                onBackPressedCallback = onBackPressedCallback()
+            )
+        }
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(menuProvider(), viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun onBackPressedCallback() = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (viewModel.isContainsSelected()) {
+                viewModel.unselectPokemon()
+            } else if (!searchView.isIconified) {
+                searchView.onActionViewCollapsed()
+            } else {
+                requireActivity().finish()
+            }
+        }
+    }
+
+    private fun menuProvider() = object : MenuProvider {
+        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+            menuInflater.inflate(R.menu.search_menu, menu)
+            val item: MenuItem? = menu.findItem(R.id.action_search)
+            if (item != null) {
+                _searchView = item.actionView as SearchView
+                setUpSearchView(searchView)
+            }
+        }
+
+        override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+            return when (menuItem.itemId) {
+                R.id.action_search -> {
+                    viewModel.unselectPokemon()
+                    true
+                }
+                else -> { false }
+            }
+        }
+    }
+
+    private fun setUpSearchView(menuItem: SearchView) {
+        with(menuItem) {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    viewModel.onSearchTextChange(query ?: "")
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    viewModel.onSearchTextChange(newText ?: "")
+                    return true
+                }
+            })
+            setOnSearchClickListener {
+                viewModel.unselectPokemon()
+            }
         }
     }
 
@@ -85,14 +154,21 @@ class PokemonListFragment : Fragment(), UserActionListener  {
     }
 
     override fun onOpen(pokemonItem: PokemonItem) {
-        val args = bundleOf(PokemonDetailsFragment.ARG_ID to pokemonItem.id)
-        findNavController().navigate(
-            resId = R.id.action_pokemonListFragment_to_pokemonDetailsFragment,
-            args = args
-        )
+        if (viewModel.isContainsSelected()) {
+            viewModel.selectPokemon(pokemonItem)
+        } else {
+            val args = bundleOf(PokemonDetailsFragment.ARG_ID to pokemonItem.id)
+            findNavController().navigate(
+                resId = R.id.action_pokemonListFragment_to_pokemonDetailsFragment,
+                args = args
+            )
+        }
     }
 
-    override fun onSelect(pokemonItem: PokemonItem) {
-        adapter.selectPokemon(pokemonItem)
+    override fun onSelect(pokemonItem: PokemonItem) = viewModel.selectPokemon(pokemonItem)
+
+    override fun onStop() {
+        viewModel.unselectPokemon()
+        super.onStop()
     }
 }
