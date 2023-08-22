d76c9d6904cbb0c5cb0a44fbf4c7ffebcd807162
bashlikovvv <volodia413314@gmail.com>
2023-08-22 15:15:48 +0300

    PokemonActivity code cleaned.

--- app/src/main/java/by/bashlikovvv/pokemon/presentation/view/PokemonActivity.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/presentation/view/PokemonActivity.kt
@@ -1,11 +1,7 @@
 package by.bashlikovvv.pokemon.presentation.view
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.DrawableCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,8 +9,6 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import by.bashlikovvv.pokemon.R
 import by.bashlikovvv.pokemon.databinding.ActivityPokemonBinding
-import by.bashlikovvv.pokemon.presentation.contract.CustomAction
-import by.bashlikovvv.pokemon.presentation.contract.HasCustomAction
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -36,35 +30,6 @@ class PokemonActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        updateUi()
-        return true
-    }
-
-    private fun updateUi() {
-        val fragment = supportFragmentManager.findFragmentById(R.id.fragment_container)!!
-
-        if (fragment is HasCustomAction) {
-            createCustomToolbarAction(fragment.getCustomAction())
-        } else {
-            binding.toolbar.menu.clear()
-        }
-    }
-
-    private fun createCustomToolbarAction(action: CustomAction) {
-        binding.toolbar.menu.clear()
-
-        val iconDrawable = DrawableCompat.wrap(ContextCompat.getDrawable(this, action.iconRes)!!)
-
-        val menuItem = binding.toolbar.menu.add(action.textRes)
-        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-        menuItem.icon = iconDrawable
-        menuItem.setOnMenuItemClickListener {
-            action.onCustomAction.run()
-            return@setOnMenuItemClickListener true
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
