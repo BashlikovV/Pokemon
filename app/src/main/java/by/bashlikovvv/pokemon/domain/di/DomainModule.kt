9c417a1a90060220ac594c5995c66f7785952c3a
bashlikovvv <volodia413314@gmail.com>
2023-08-22 15:14:40 +0300

    UpdatePokemonSelectedUseCase and UnselectPokemonUseCase providers added.

--- app/src/main/java/by/bashlikovvv/pokemon/domain/di/DomainModule.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/domain/di/DomainModule.kt
@@ -2,9 +2,12 @@ package by.bashlikovvv.pokemon.domain.di
 
 import by.bashlikovvv.pokemon.data.repository.PokemonDetailsRepository
 import by.bashlikovvv.pokemon.data.repository.PokemonListRepository
+import by.bashlikovvv.pokemon.data.repository.PokemonStateRepository
 import by.bashlikovvv.pokemon.domain.usecase.GetPokemonByNameUseCase
 import by.bashlikovvv.pokemon.domain.usecase.GetPokemonListUseCase
 import by.bashlikovvv.pokemon.domain.usecase.GetPokemonDetailsByIdUseCase
+import by.bashlikovvv.pokemon.domain.usecase.UnselectPokemonUseCase
+import by.bashlikovvv.pokemon.domain.usecase.UpdatePokemonUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,5 +30,15 @@ class DomainModule {
     @Provides
     fun providePokemonByNameUseCase(pokemonListRepository: PokemonListRepository): GetPokemonByNameUseCase {
         return GetPokemonByNameUseCase(pokemonListRepository)
+    }
+
+    @Provides
+    fun provideUpdatePokemonSelectedUseCase(pokemonStateRepository: PokemonStateRepository): UpdatePokemonUseCase {
+        return UpdatePokemonUseCase(pokemonStateRepository)
+    }
+
+    @Provides
+    fun provideUnselectPokemonUseCase(pokemonStateRepository: PokemonStateRepository): UnselectPokemonUseCase {
+        return UnselectPokemonUseCase(pokemonStateRepository)
     }
 }
