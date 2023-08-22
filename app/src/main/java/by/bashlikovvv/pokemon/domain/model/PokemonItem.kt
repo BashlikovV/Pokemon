e98725cecfcd49647d4f565ab1dd1f162e5a73fa
bashlikovvv <volodia413314@gmail.com>
2023-08-22 15:16:00 +0300

    isEmpty method added.

--- app/src/main/java/by/bashlikovvv/pokemon/domain/model/PokemonItem.kt
+++ app/src/main/java/by/bashlikovvv/pokemon/domain/model/PokemonItem.kt
@@ -3,5 +3,15 @@ package by.bashlikovvv.pokemon.domain.model
 data class PokemonItem(
     val name: String = "",
     val id: Int = 0,
-    var sprite: String = ""
-)
+    var sprite: String = "",
+    var selected: Boolean = false
+) {
+
+    fun isEmpty(): Boolean {
+        if (name.isNotEmpty()) return false
+        if (sprite.isNotEmpty()) return false
+        if (selected) return false
+
+        return id != 0
+    }
+}
